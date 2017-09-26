ff1da64f68c297d9307ebee8644f56e5803a287f
ian.r <Abcd1234>
2017-09-26 19:28:40 +0800

    Added app.json, index.js and package.json

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const bodyParser = require('body-parser');
 
 const restService = express();
 
+const BOOKING_SEARCH = 'booking/search';
+const BOOKING_ROUND = 'booking/search';
+const BOOKING_ONEWAY = 'booking/search';
+const BOOKING_FROM = 'booking/search';
+const BOOKING_TO = 'booking/search';
+const BOOKING_DATE2 = 'booking/search';
+
+
+const DETAILS_CONTACT_INFO = 'details/contactInfo';
+const DETAILS_PASSENGER = 'details/passenger';
+const DETAILS_SUMMARY2 = 'details/summary2';
+const DETAILS_SUMMARY1 = 'details/summary1';
+
+
+const FAQ_FCONTACT = 'faq/fcontact';
+const FAQ_FAQ = 'faq/faq';
+const FAQ_FPAYMENT= 'faq/fpayment';
+
 restService.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -14,6 +32,12 @@ restService.use(bodyParser.json());
 
 //Main code here
 restService.post('/webhook', function(req, res) {
+
+});
+restService.post('/details', function(req, res) {
+
+});
+restService.post('/faq', function(req, res) {
 
 });
 //Main code end
